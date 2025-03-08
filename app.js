327cdf5f0d3ce6eaa648989e51b77d379ed28fcb
PatelPratik39 <ptl.pratik717@gmail.com>
2025-03-07 20:37:29 -0600

    Database configured

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { PORT } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import connectionToDaatabase from "./database/mongodb.js";
 
 const app = express();
 app.use(express.json());
@@ -15,9 +16,9 @@ app.get("/", (req, res) => {
   res.send("Welcome to Subscription Tracker Backend API");
 });
 
-app.listen(PORT, () => {
-  console.log(
-    `SubScription Tracker Backend API is running on http://localhost:${PORT} in ${process.env.NODE_ENV} mode`
-  );
+app.listen(PORT, async() => {
+  console.log(`SubScription Tracker Backend API is running on http://localhost:${PORT}`);
+  await connectionToDaatabase();
 });
+
 export default app;
