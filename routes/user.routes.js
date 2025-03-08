1397b5e09f49ae11b2a8cee091317df3492826cd
PatelPratik39 <ptl.pratik717@gmail.com>
2025-03-08 16:38:35 -0600

    JWT authentication and authorize middleware

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,11 @@
 import e, { Router } from "express";
 import { getUser, getUsers } from "../controller/user.controller.js";
+import  authorized  from "../middleware/auth.middleware.js";
 
 const userRouter = Router();
 
 userRouter.get("/", getUsers);
-userRouter.get("/:id", getUser);
+userRouter.get("/:id", authorized, getUser);
 userRouter.post("/", (req, res) => res.send({ title: "CREATE New User" }));
 userRouter.put("/:id", (req, res) => res.send({ title: "UPDATE a Users" }));
 userRouter.delete("/:id", (req, res) => res.send({ title: "DELETE a User" }));
